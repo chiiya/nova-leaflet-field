11fb033ef58b5cce56ca0a6e2b536e413c26b370
Elisha Witte <elisha.witte@antwerpes.de>
2021-07-22 15:33:59 +0200

    Build components

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ let mix = require('laravel-mix')
 
 mix
   .setPublicPath('dist')
-  .js('resources/js/field.js', 'js')
+  .js('resources/js/field.js', 'js').vue()
   .sass('resources/sass/field.scss', 'css')
